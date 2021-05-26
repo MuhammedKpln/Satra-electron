1016db68bdad3b6545b222f050ab1612566c2b91
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-05-26 22:11:10 +0200

    update

--- src/menu.ts
+++ src/menu.ts
@@ -7,14 +7,20 @@ const hardRestart = async () => {
   await session.defaultSession.clearHostResolverCache();
   await session.defaultSession.clearCache();
   app.relaunch();
+  app.exit(0);
+  app.quit();
   console.log("Hard restarted");
 };
 
 const update = async () => {
-  await autoUpdater.checkForUpdatesAndNotify({
+  const update = await autoUpdater.checkForUpdatesAndNotify({
     title: "Uppdatering tillgänglig!",
     body: "Uppdatering tillgänglig för Sätra Trafikskola.",
   });
+
+  if (!update) {
+    dialog.showErrorBox("Finns inga uppdatering", "");
+  }
 };
 
 export const menuTemplate = [
